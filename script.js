c4909e73090ba31d52c30809e6a2b87a2c5552f0
Lemonade0316 <henrytcl4work@gmail.com>
2024-11-04 22:31:38 +0800

    Added Take Pic/html/css

--- script.js
+++ script.js
@@ -1,66 +1,52 @@
-const imageUpload = document.getElementById('imageUpload')
-const camera = document.getElementById('video');
-const captureButton = document.getElementById('takeImage');
+const video = document.getElementById('video');
+const canvas = document.getElementById('canvas');
+const context = canvas.getContext('2d');
+const captureButton = document.getElementById('captureButton');
 
-navigator.mediaDevices.getUserMedia({video: {facingMode: "environment"}})
-    .then(function(stream){
-      camera.srcObject = stream;
+navigator.mediaDevices.getUserMedia({ video: { facingMode: "environment" } })
+    .then(function(stream) {
+        video.srcObject = stream;
+        video.width = 500;
+        video.height = 500;
     })
-    .catch(function(error){
-      console.log("Error accessing the camera: " + error);
-    })
+    .catch(function(error) {
+        console.log("Error accessing the camera: " + error);
+    });
 
-captureButton.addEventListener('click', function(){
-  //make the imageUpload become the frame of the camera when captured
-})
+getLocation()
 
-Promise.all([
-  faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
-  faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
-  faceapi.nets.ssdMobilenetv1.loadFromUri('/models')
-]).then(start)
+// Capture an image when the button is clicked
+captureButton.addEventListener('click', function() {
+    // Set canvas size to video size
+    canvas.width = video.width;
+    canvas.height = video.height;
 
-async function start() {
-  const container = document.createElement('div')
-  container.style.position = 'relative'
-  document.body.append(container)
-  const labeledFaceDescriptors = await loadLabeledImages()
-  const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors, 0.6)
-  let image
-  let canvas
-  document.body.append('Loaded')
-  imageUpload.addEventListener('change', async () => {
-    if (image) image.remove()
-    if (canvas) canvas.remove()
-    image = await faceapi.bufferToImage(imageUpload.files[0])
-    container.append(image)
-    canvas = faceapi.createCanvasFromMedia(image)
-    container.append(canvas)
-    const displaySize = { width: image.width, height: image.height }
-    faceapi.matchDimensions(canvas, displaySize)
-    const detections = await faceapi.detectAllFaces(image).withFaceLandmarks().withFaceDescriptors()
-    const resizedDetections = faceapi.resizeResults(detections, displaySize)
-    const results = resizedDetections.map(d => faceMatcher.findBestMatch(d.descriptor))
-    results.forEach((result, i) => {
-      const box = resizedDetections[i].detection.box
-      const drawBox = new faceapi.draw.DrawBox(box, { label: result.toString() })
-      drawBox.draw(canvas)
-    })
-  })
+    // Draw the current video frame on the canvas
+    context.drawImage(video, 0, 0, canvas.width, canvas.height);
+
+    // Show the canvas (captured image)
+    canvas.style.display = 'block';
+
+    // Optionally, get the image data URL (if you want to upload it, for example)
+    const imageDataUrl = canvas.toDataURL('image/png');
+    console.log(imageDataUrl);  // You can use this data URL to upload the image
+
+});
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        console.error("Geolocation is not supported by this browser.");
+    }
 }
 
-function loadLabeledImages() {
-  const labels = ['Black Widow', 'Captain America', 'Captain Marvel', 'Hawkeye', 'Jim Rhodes', 'Thor', 'Tony Stark']
-  return Promise.all(
-    labels.map(async label => {
-      const descriptions = []
-      for (let i = 1; i <= 2; i++) {
-        const img = await faceapi.fetchImage(`https://raw.githubusercontent.com/SteebNg/softwareEnginAssign/master/labeled_images/${label}/${i}.jpg`)
-        const detections = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor()
-        descriptions.push(detections.descriptor)
-      }
+function showPosition(position) {
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
 
-      return new faceapi.LabeledFaceDescriptors(label, descriptions)
-    })
-  )
+    // Display the latitude and longitude below the canvas
+    const locationDisplay = document.createElement('p');
+    locationDisplay.textContent = 'Latitude: ${latitude}, Longitude: ${longitude}';
+    document.body.appendChild(locationDisplay);
 }
