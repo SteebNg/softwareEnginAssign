684e3b2ceb501cc567c36aeb8639d5c078e86aa7
NgWeiCheng <92345739+SteebNg@users.noreply.github.com>
2024-11-19 00:59:30 +0800

    Face Recog

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
-// const imageUpload = document.getElementById('imageUpload')
-const video = document.getElementById('video');
-const canvasPic = document.getElementById('canvas');
-const context = canvasPic.getContext('2d');
-const captureButton = document.getElementById('captureButton');
+const imageUpload = document.getElementById('imageUpload')
 
 Promise.all([
   faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
@@ -10,36 +6,25 @@ Promise.all([
   faceapi.nets.ssdMobilenetv1.loadFromUri('/models')
 ]).then(start)
 
-navigator.mediaDevices.getUserMedia({ video: { facingMode: "environment" } })
-    .then(function(stream) {
-        video.srcObject = stream;
-        video.width = 500;
-        video.height = 500;
-    })
-    .catch(function(error) {
-        console.log("Error accessing the camera: " + error);
-    });
-
 getLocation()
 
-// Capture an image when the button is clicked
-captureButton.addEventListener('click', function() {
-    // Set canvas size to video size
-    canvasPic.width = 500;
-    canvasPic.height = 500;
-
-    // Draw the current video frame on the canvas
-    context.drawImage(video, 0, 0, canvasPic.width, canvasPic.height);
-
-    // Show the canvas (captured image)
-    canvasPic.style.display = 'block';
-
-
-    // Optionally, get the image data URL (if you want to upload it, for example)
-    const imageDataUrl = canvasPic.toDataURL('image/png');
-    console.log(imageDataUrl);  // You can use this data URL to upload the image
-
-});
+// // Capture an image when the button is clicked
+// captureButton.addEventListener('click', function() {
+//     // Set canvas size to video size
+//     canvas.width = video.width;
+//     canvas.height = video.height;
+//
+//     // Draw the current video frame on the canvas
+//     context.drawImage(video, 0, 0, canvas.width, canvas.height);
+//
+//     // Show the canvas (captured image)
+//     canvas.style.display = 'block';
+//
+//     // Optionally, get the image data URL (if you want to upload it, for example)
+//     const imageDataUrl = canvas.toDataURL('image/png');
+//     console.log(imageDataUrl);  // You can use this data URL to upload the image
+//
+// });
 
 function getLocation() {
     if (navigator.geolocation) {
@@ -50,27 +35,27 @@ function getLocation() {
 }
 
 function showPosition(position) {
-    const latitude = position.coords.latitude;
-    const longitude = position.coords.longitude;
-    console.log(latitude, longitude);
+  const latitude = position.coords.latitude;
+  const longitude = position.coords.longitude;
+  document.getElementById("output").innerHTML = `Latitude: ${latitude}, Longitude: ${longitude}`;
 }
 
 async function start() {
-  // const container = document.createElement('div')
-  // container.style.position = 'relative'
-  // document.body.append(container)
+  const container = document.createElement('div')
+  container.style.position = 'relative'
+  document.body.append(container)
   const labeledFaceDescriptors = await loadLabeledImages()
   const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors, 0.6)
   let image
   let canvas
   document.body.append('Loaded')
-  canvasPic.addEventListener('change', async () => {
+  imageUpload.addEventListener('change', async () => {
     if (image) image.remove()
     if (canvas) canvas.remove()
-    image = await faceapi.bufferToImage(canvasPic)
-    // container.append(image)
+    image = await faceapi.bufferToImage(imageUpload.files[0])
+    container.append(image)
     canvas = faceapi.createCanvasFromMedia(image)
-    // container.append(canvas)
+    container.append(canvas)
     const displaySize = { width: image.width, height: image.height }
     faceapi.matchDimensions(canvas, displaySize)
     const detections = await faceapi.detectAllFaces(image).withFaceLandmarks().withFaceDescriptors()
@@ -80,7 +65,6 @@ async function start() {
       const box = resizedDetections[i].detection.box
       const drawBox = new faceapi.draw.DrawBox(box, { label: result.toString() })
       drawBox.draw(canvas)
-        console.log(result.toString())
     })
   })
 }
@@ -91,7 +75,7 @@ function loadLabeledImages() {
     labels.map(async label => {
       const descriptions = []
       for (let i = 1; i <= 2; i++) {
-        const img = await faceapi.fetchImage("https://raw.githubusercontent.com/WebDevSimplified/Face-Recognition-JavaScript/master/labeled_images/${label}/${i}.jpg")
+        const img = await faceapi.fetchImage(`https://raw.githubusercontent.com/WebDevSimplified/Face-Recognition-JavaScript/master/labeled_images/${label}/${i}.jpg`)
         const detections = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor()
         descriptions.push(detections.descriptor)
       }
