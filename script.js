0df188f412a17af6cacd37272ae7ba0f5f7d2180
Waynenkl <waynengkl@gmail.com>
2024-11-18 22:02:36 +0800

    added capture button

--- script.js
+++ script.js
@@ -6,6 +6,34 @@ Promise.all([
   faceapi.nets.ssdMobilenetv1.loadFromUri('/models')
 ]).then(start)
 
+getLocation()
+
+// Capture an image when the button is clicked
+captureButton.addEventListener('click', function() {
+    // Set canvas size to video size
+    canvas.width = video.width;
+    canvas.height = video.height;
+
+    // Draw the current video frame on the canvas
+    context.drawImage(video, 0, 0, canvas.width, canvas.height);
+
+    // Show the canvas (captured image)
+    canvas.style.display = 'block';
+
+    // Optionally, get the image data URL (if you want to upload it, for example)
+    const imageDataUrl = canvas.toDataURL('image/png');
+    console.log(imageDataUrl);  // You can use this data URL to upload the image
+
+});
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        console.error("Geolocation is not supported by this browser.");
+    }
+}
+
 async function start() {
   const container = document.createElement('div')
   container.style.position = 'relative'
